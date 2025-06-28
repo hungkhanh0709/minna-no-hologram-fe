ec4a4257cc0c7484b2d8842490d81405a54d5bf2
tnbngoc <tnbngoc912@gmail.com>
2025-06-28 10:46:52 +0700

    update api

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,11 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const API_VERSION = "/v1";
 
 export const API_URLS = {
   HOME: `${BASE_URL}${API_VERSION}/home`,
   SEARCH: `${BASE_URL}${API_VERSION}/search`,
+  //categories
   VIDEO_SEARCH: `${BASE_URL}${API_VERSION}/video`,
   VIDEO_DETAIL: (slugId: string) => `${BASE_URL}${API_VERSION}/video/${slugId}`,
   DIY_SEARCH: `${BASE_URL}${API_VERSION}/diy`,
